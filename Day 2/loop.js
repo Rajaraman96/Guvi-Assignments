34e80485a82ebf60952fb21997f23000e650ed42
Rajaraman96 <92200766+Rajaraman96@users.noreply.github.com>
2021-12-14 08:35:00 +0530

    For loops

---
+++ Day 2/loop.js
@@ -0,0 +1,43 @@
+//For Loop
+
+var obj = [
+    { person: "Name 1", age: "2", company: "GUVI" },
+    { person: "Name 2", age: "5", company: "GUVI geek" },
+    { person: "Name 3", age: "8", company: "GUVI geek network" },
+  ];
+  for (let i = 0; i < obj.length; i++  ){
+      console.log(obj[i]);
+  }
+
+  //For In
+  var obj = [
+    { person: "Name 1", age: "2", company: "GUVI" },
+    { person: "Name 2", age: "5", company: "GUVI geek" },
+    { person: "Name 3", age: "8", company: "GUVI geek network" },
+  ];
+  let b=""
+  for (let x in obj){
+      console.log(obj[x]);
+  }
+
+
+  //For Of
+  var obj = [
+    { person: "Name 1", age: "2", company: "GUVI" },
+    { person: "Name 2", age: "5", company: "GUVI geek" },
+    { person: "Name 3", age: "8", company: "GUVI geek network" },
+  ];
+  let i=0
+ for(let x of obj){
+     i += obj[x];
+     console.log(x)
+ }
+
+ //For Each 
+
+ var fruits = obj = [
+    { person: "Name 1", age: "2", company: "GUVI" },
+    { person: "Name 2", age: "5", company: "GUVI geek" },
+    { person: "Name 3", age: "8", company: "GUVI geek network" },
+  ];
+fruits.forEach(element => console.log(element));
