2eb252f67b5753c042cde2a2e6769c1dfac23488
Rajaraman96 <92200766+Rajaraman96@users.noreply.github.com>
2021-12-26 14:30:56 +0530

    Day 5

--- Day 5/Arrow functions.js
+++ Day 5/Arrow functions.js
@@ -1,67 +1,78 @@
-// Print odd numbers in an array
-// Convert all the strings to title caps in a string array
-// Sum of all numbers in an array
-// Return all the prime numbers in an array
-// Return all the palindromes in an array 
 
 //1. Print odd numbers in an array
 
-let numberArray = [1, 2, 3, 4, 5, 6, 98, 101, 467]
+        let numberArray = [1, 2, 3, 4, 5, 6, 98, 101, 467]
 
-console.log("Odd Numbers in Number Array are: " + numberArray.filter((item) => {
+        numberArray.filter((item) => {
 
-    return item % 2 !== 0
+            return item % 2 !== 0;
+            console.log("Odd Numbers in Number Array are: " + numberArray)
 
-}))
+        });
+//-------Output:-------
+// 0: 1
+// 1: 3
+// 2: 5
+// 3: 101
+// 4: 467
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
 
 //2.Convert all the strings to title caps in a string array
 
-let stringArray = ["Akshay", "is", "a", "smart", "developer"]
+        let stringArray = ["hi","rajaraman","welcome","to","guvi."]
+        stringArray = stringArray.map((item) => {
+            return item.toUpperCase()
+        })
+        console.log(stringArray.join(" "))// .join will joins the data in an array //Output: HI RAJARAMAN WELCOME TO GUVI.
 
-stringArray = stringArray.map((item) => {
 
-    return item.toUpperCase()
-
-})
-
-console.log("Converted String Array is: " + stringArray)
+//------------------------------------------------------------------------------------------------------------------------------------------
 
 //3.Sum of all numbers in an array
-//using numArray Defined Above
 
-let sum = numberArray.reduce((currentTotal, item) => {
 
-    return currentTotal + item
+        let numberArray = [1, 2, 3, 4, 5, 6, 98, 101, 467]
+        let sum = numberArray.reduce((currentTotal, item) => {
 
-}, 0)
+            return currentTotal + item
 
-console.log("Sum of elements in Number Array: " + sum)
+        }, 0)
+
+        console.log("Sum of elements in Number Array: " + sum) //Output: Sum of elements in Number Array: 687
+
+//------------------------------------------------------------------------------------------------------------------------------------------
 
 //4.Return all the prime numbers in an array
 
-console.log("Prime Number is Array are: " +
-    numberArray.filter((item) => {
+        let numberArray = [1, 2, 3, 4, 5, 6, 98, 101, 467]
+        console.log("Prime Number is Array are: " +
+            numberArray.filter((item) => {
 
-        for (let i = 2; i < item; i++) {
-            if (item % i === 0)
-                return false;
-        }
-        return item !== 1;
+                for (let i = 2; i < item; i++) {
+                    if (item % i === 0)
+                        return false;
+                }
+                return item !== 1;
 
-    }))
+            })) //Output: Prime Number is Array are: 2,3,5,101,467
+
+//------------------------------------------------------------------------------------------------------------------------------------------
 
 //5.Return all the palindromes in an array 
 
-let arrayPallindrome = [111, 102, 505, 323, 612, 776, 707, 121]
+        let arrayPallindrome = [111, 102, 505, 323, 612, 776, 707, 121]
 
-console.log("Pallindrome Numbers in array are: " +
-    arrayPallindrome.filter((item) => {
+        console.log("Pallindrome Numbers in array are: " +
+            arrayPallindrome.filter((item) => {
 
-        let temp = item + "";
+                let temp = item + "";
 
-        if (temp.split('').reverse().join('') === item + "")
-            return true
+                if (temp.split('').reverse().join('') === item + "")
+                    return true
 
-        return false
+                return false
 
-    }))
+            })) //Output: Pallindrome Numbers in array are: 111,505,323,707,121
+//------------------------------------------------------------------------------------------------------------------------------------------
--- Day 5/Medium Problems.js
+++ Day 5/Medium Problems.js
@@ -1,8 +1,9 @@
 // Write a function called “addFive”.
 // Given a number, “addFive” returns 5 added to that number.
-        let addFive = (num) => {
+        let num = 0;
+        function addFive(num){
             return num + 5
-        }
+        }addFive(num);
 
         console.log(addFive(5))  //10
         console.log(addFive(0))  //5
@@ -36,13 +37,13 @@
 
 // Fill in your code that takes an number minutes and converts it to seconds.
 
-let toSeconds = (min) => {
-    return min * 60
-}
+        let toSeconds = (min) => {
+            return min * 60
+        }
 
-console.log(`Converted value of minute to seconds: ${toSeconds(5)}sec `);   //Converted value of minute to seconds: 300 secs
-console.log(`Converted value of minute to seconds: ${toSeconds(3)}sec `);   //Converted value of minute to seconds: 180 secs
-console.log(`Converted value of minute to seconds: ${toSeconds(2)}sec `);   //Converted value of minute to seconds: 120 secs
+        console.log(`Converted value of minute to seconds: ${toSeconds(5)}sec `);   //Converted value of minute to seconds: 300 secs
+        console.log(`Converted value of minute to seconds: ${toSeconds(3)}sec `);   //Converted value of minute to seconds: 180 secs
+        console.log(`Converted value of minute to seconds: ${toSeconds(2)}sec `);   //Converted value of minute to seconds: 120 secs
 
 //------------------------------------------------------------------------------------------------------------------------------------------        
 
@@ -86,11 +87,13 @@ console.log(`Converted value of minute to seconds: ${toSeconds(2)}sec `);   //Co
 // Convert Hours into Seconds
 // Write a function that converts hours into seconds.
 
-let hourToSeconds = (hour) => {
-    return hour * 60 *60
-}
+        let hourToSeconds = (hour) => {
+            return hour * 60 *60
+        }
 
-console.log(`Converted value of hour to second: ${hourToSeconds(2)} secs`)
+        console.log(`Converted value of hour to second: ${hourToSeconds(2)} secs`) 
+        //Output: Converted value of hour to second: 7200 secs
+
 
 //-------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -117,13 +120,159 @@ console.log(`Converted value of hour to second: ${hourToSeconds(2)} secs`)
             }
             sum(99,2); //False
             sum(99,1); //True
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+// There is a single operator in JavaScript, capable of providing the remainder of a division operation. 
+// Two numbers are passed as parameters. 
+// The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
+
+        let remainder = (a,b) => {
+            return a%b
+        }
+        console.log(`Remainder for the division of given number is: ${remainder(-9, 45)} ` )
+        //Output : Remainder for the division of given number is: -9 
 
 
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+//McDonald species and its legs
+//turkey = a, horse = b,pigs = c
+
+
+        let legsOfSpecies = (a,b,c) => {
+            return  ((a*2)+(b*4)+(c*4))
+        }
+            
+        console.log(`Total no: of legs for whole species are : ${legsOfSpecies(5,6,7)}`)
+        //Output: Total no: of legs for whole species are : 62
+
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+//Frames Per Second
+//Create a function that returns the number of frames shown in a given number of minutes for a certain FPS
+
+
+    let numberOfFrames = (a,b) => {
+        return a*b*60
+    }
+    console.log(`Number of frames in a given minute is ${numberOfFrames(6,5)}`)
+    //Output: Number of frames in a given minute is 1800
+
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+//Check if an Integer is Divisible By Five
+//Create a function that returns true if an integer is evenly divisible by 5, and false otherwise.
+        let divisibleByFive = (num) =>{
+            if((num%5)== 0){
+                return true
+            }else{
+                return false
+            }
+        }
+            console.log(divisibleByFive(55)); //True
+            console.log(divisibleByFive(101)) //False
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+//Write a function called “isEven”.
+//Given a number, “isEven” returns whether it is even.
+            let num = 0;        
+            function isEven(num){
+                if(num%2 === 0){
+                    return true
+                }
+                if (Number.isInteger(num) && (num%2 !==0 )) { //checks, data  is an Integer or not and also checks the remainder is not 0
+                    return false           //return's False
+                }                
+                else{
+                    return -1
+                }
+            }isEven(num);
+            console.log(isEven(5));     //False
+            console.log(isEven("11h"))  //-1
+            console.log(isEven(6))      //True
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+//Write a function called “areBothOdd”.
+//Given 2 numbers, “areBothOdd” returns whether or not both of the given numbers are odd.
+
+            let a,b = 0;
+            function areBothOdd(a,b){
+                if (a%2 !== 0 && b%2 !== 0){
+                    return true
+                }else{
+                    return false
+                }
+            }
+            console.log(areBothOdd(5,6)); //False
+            console.log(areBothOdd(1,7)); //True
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+//Write a function called “getFullName”.
+//Given a first and a last name, “getFullName” returns a single string with the given first and last names separated by a single space.
+            let fname,lname;
+            function getFullName(fname,lname){
+                
+                return console.log(" "+fname+ " " +lname+" ");
+            }getFullName("Raja","Raman");
+            // Output:  Raja Raman 
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+//Write a function called “getLengthOfWord”.
+//Given a word, “getLengthOfWord” returns the length of the given word.
+            let inputString = "";
+            function getLengthOfWord(inputString){
+                if(typeof inputString === 'string') 
+                {
+                    return inputString.length
+                } 
+                    return -1
+                }
+                
+            getLengthOfWord(inputString);
+
+            console.log(getLengthOfWord("Maravaazha")) //10
+            console.log(getLengthOfWord(8))            //-1
+            console.log(getLengthOfWord())             //-1
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+//Write a function called “isSameLength”.
+//Given two words, “isSameLength” returns whether the given words have the same length.
+            let word1= "";
+            let word2="";
+            function isSameLength(word1, word2){
+                if(word1.length == word2.length){
+                    return true
+                }return "Words does not have same length"
+            }isSameLength(word1, word2)
+
+            console.log(isSameLength("malar","malar")) // true
+            console.log(isSameLength("thenga","karikku"))// Words does not have same length
 
 
 
 //------------------------------------------------------------------------------------------------------------------------------------------
+// Create a function to calculate the distance between two points defined by their x, y coordinates
+            let array =[];
+            let n=0;
+
+            function getNthElement(array,n){
+                return  array[n]
+
+            }getNthElement(array,n)
+
+            console.log(getNthElement([1,3,5,7,9],3))  //7
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
 // Calculate the sum of numbers received in a comma delimited string
+            console.log(isEven(6))      //True
 
         let str = '1.2,2.3,3.4,4.5,5.6,6.7,7.8,8.9'
 
@@ -144,4 +293,4 @@ console.log(`Converted value of hour to second: ${hourToSeconds(2)} secs`)
         console.log("Sum of string array: " + sumOfStringArray(str)) //Sum of string array: 40.4
 
 //------------------------------------------------------------------------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------END-------------------------------------------------------------------------------------
